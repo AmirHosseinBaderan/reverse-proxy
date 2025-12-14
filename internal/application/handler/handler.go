61e796a093215a218f37b636cc680450388e96ce
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-12-14 18:57:28 +0330

    create : host handler

---
+++ internal/application/handler/handler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+)
+
+func HostRouter(sites map[string]http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		host := strings.Split(r.Host, ":")[0] // remove port
+
+		if h, ok := sites[host]; ok {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		http.NotFound(w, r)
+	})
+}
